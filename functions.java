7350f2d55b4ece29ecb723c5cb490453276a91f8
Sain-21 <painkiller9089@gmail.com>
2024-06-12 18:30:42 +0300

    funcions file

--- functions.java
+++ functions.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class functions {
